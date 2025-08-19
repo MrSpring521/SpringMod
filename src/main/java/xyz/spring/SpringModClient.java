5d08ad7d507141f20b3dee16409851b04875f84e
MrSpring521 <private@springmc.xyz>
2025-08-19 15:30:50 +0800

    Init

---
+++ src/main/java/xyz/spring/SpringModClient.java
@@ -0,0 +1,10 @@
+package xyz.spring;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpringModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
